668ea0692f6066cae161448639e6b677e78facdd
jcocozza <jcocozza@gmail.com>
2023-01-06 19:33:57 +0000

    login, logout and register features

--- scripts/flask_app.py
+++ scripts/flask_app.py
@@ -2,12 +2,17 @@ from flask import Flask, render_template, Markup, redirect, request, url_for,ses
 import sql_functions
 import get_user_activity_data
 from get_user_activity_data import approval_link
+import pandas as pd
 import os
 
 cwd = os.getcwd()
 
 app = Flask(__name__)
 app.secret_key = 'blahblahblahblahblah'
+
+@app.route('/')
+def enter():
+    return redirect(url_for('welcome_page'))
 
 @app.route('/login', methods=['GET','POST'])
 def login():
@@ -23,24 +28,70 @@ def login():
         query = """SELECT * FROM users WHERE username = '%s' AND password = '%s';""" % (username, password)
         result = sql_functions.local_sql_to_df(query)
 
-
-        # If account exists in accounts table in out database
+        # If account exists in user table in out database
         if not result.empty:
             # Create session data, we can access this data in other routes
             session['loggedin'] = True
             session['id'] = int(result['user_id'][0])
             session['username'] = str(result['username'][0])
             # Redirect to home page
-            return redirect('/', code=302)
+            return redirect('/home', code=302)
     else:
         # Account doesnt exist or username/password incorrect
         msg = 'Incorrect username/password!'
 
     return render_template('login.html', msg=msg)
 
-@app.route('/')
+@app.route('/logout')
+def logout():
+    # Remove session data, this will log the user out
+   session.pop('loggedin', None)
+   session.pop('id', None)
+   session.pop('username', None)
+   # Redirect to login page
+   return redirect(url_for('login'))
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    # Output message if something goes wrong...
+    msg = ''
+    # Check if "username", "password" and "email" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        # Create variables for easy access
+        username = request.form['username']
+        password = request.form['password']
+
+        # Check if account exists; if it doesn't insert info into the table
+        query = """SELECT * FROM users WHERE username = '%s' AND password = '%s';""" % (username, password)
+        result = sql_functions.local_sql_to_df(query)
+        # If account exists in user table
+        if not result.empty:
+            msg = 'Account already exists!'
+        elif not username or not password:
+            msg = 'Please fill out the form!'
+        else:
+            data = {
+                'username': username,
+                'password': password,
+                'athlete_id': None,
+                'refresh_token': None
+                }
+            df = pd.DataFrame(data, index=[0])
+            sql_functions.df_to_local_sql(df, 'users')
+            msg = 'Successfully Registered'
+
+    elif request.method == 'POST':
+        # Form is empty... (no POST data)
+        msg = 'Please fill out the form!'
+    # Show registration form with message (if any)
+    return render_template('register.html', msg=msg)
+
+
+@app.route('/home')
 def welcome_page():
-    return render_template('home.html')
+    if 'loggedin' in session: # only see home if logged in
+        return render_template('home.html')
+    return redirect(url_for('login'))
 
 @app.route('/get_data')
 def get_approved():
@@ -54,7 +105,7 @@ def parse_request():
 
     path = cwd + '/data/data.csv'
     sql_functions.upload_data_file_to_local(path, 'strava_app_activity_data')
-    return redirect('/')
+    return redirect('/home')
 
 @app.route('/summary_data')
 def summarize():
