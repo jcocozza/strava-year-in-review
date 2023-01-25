9427623591dd90af3972d8383c4a47132a45e500
jcocozza <jcocozza47@gmail.com>
2023-01-25 14:50:49 +0000

    all graphs now render properly for an individual activity(with a distance stream)

--- scripts/single_activity_analysis.py
+++ scripts/single_activity_analysis.py
@@ -1,8 +1,6 @@
 import plotly.express as px
-import plotly.express as px
 import pandas as pd
 import plotly.graph_objects as go
-from plotly.subplots import make_subplots
 import sql_functions
 import os
 from ast import literal_eval
@@ -40,20 +38,19 @@ def zones(bin_array):
 # Binning heart rate streamed data by zone
 def heart_rate_zones(hr_data, bin_array, labels):
 
-    # Debugging:
-    print(hr_data['data'][0])
-    print('#'*10)
-    print(hr_data['data'][1])
-
-    series_data = pd.DataFrame.from_dict(data={'hr_series': literal_eval(hr_data['data'][0]), 'dt_series': literal_eval(hr_data['data'][1])})
+    series_data = pd.DataFrame.from_dict(data={'hr_series': literal_eval(hr_data['data'][1]), 'dt_series': literal_eval(hr_data['data'][0])})
+    series_data.set_index('dt_series')
     series_data['zone'] = pd.cut(series_data['hr_series'], bins=bin_array, labels=labels) # assign each HR value a zone
+
     return series_data
 
 # Bin data based on heart rate zones
 def heart_rate_bin_counts(series_data, bin_array, labels):
     count = pd.cut(series_data['hr_series'], bins=bin_array, labels=labels).value_counts().sort_index()
+
     # Return a dataframe of binned data
     binned_counts = pd.DataFrame({'zones':count.index, 'counts':count}).reset_index(drop=True)
+
     return binned_counts
 
 ########## END MANIPULATION ##########
@@ -61,7 +58,7 @@ def heart_rate_bin_counts(series_data, bin_array, labels):
 ########## PLOTS ##########
 
 def heart_rate_zone_plots(binned_counts):
-    pie = px.pie(binned_counts, values='counts', labels='index', title='Heart Rate Zone Data')
+    pie = px.pie(binned_counts, values='counts', labels='zones',names='zones', title='Heart Rate Zone Data')
     hist = px.histogram(binned_counts, x="zones", y="counts", hover_data=binned_counts.columns, title='Zone Distribution')
 
     pie.write_html(cwd + '/scripts/static/charts/hr_pie.html')
@@ -73,13 +70,28 @@ def heart_rate_zone_plots(binned_counts):
 def heart_rate_data_plot(series_data, lap_data):
     fig = px.line(series_data, x="dt_series", y="hr_series", title='Heart Rate Data', color_discrete_sequence = ['red'])
 
+    print('########## DEBUGGING ##########')
+
+    print('####### Start Index #######')
+    print(lap_data['start_index'])
+    print('####### End Index #######')
+    print(lap_data['end_index'])
+
+    print('########## END DEBUGGING ##########')
+
+
     # Add Laps to heart rate graph
-    for i, (name, begin, end, time) in enumerate(zip(lap_data['name'], lap_data['start_index'], lap_data['end_index'], lap_data['moving_time'])):
+    lap_start_dist = 0
+    for i, (name, begin, end, lap_dist) in enumerate(zip(lap_data['name'], lap_data['start_index'], lap_data['end_index'], lap_data['distance'])):
+        lap_end_dist = lap_start_dist + lap_dist
         if i % 2 == 0:
             color = 'blue'
         else:
             color = 'green'
-        fig.add_vrect(x0=begin, x1=end, line_width=0, fillcolor=color, opacity=0.2, annotation_text=name)
+        #fig.add_vrect(x0=begin, x1=end, line_width=0, fillcolor=color, opacity=0.2, annotation_text=name)
+        fig.add_vrect(x0=lap_start_dist, x1=lap_end_dist, line_width=0, fillcolor=color, opacity=0.2, annotation_text=name)
+
+        lap_start_dist = lap_end_dist
 
     fig.write_html(cwd + '/scripts/static/charts/hr_plot.html')
 
@@ -88,10 +100,10 @@ def heart_rate_data_plot(series_data, lap_data):
 
 def activity_lap_data_table(lap_data):
     tbl = go.Figure(data=[go.Table(
-    header=dict(values=['name','moving time', 'elevation gain', 'average speed', 'average heartrate', 'max heartrate'],
+    header=dict(values=['name','distance', 'moving time', 'elevation gain', 'average speed', 'average heartrate', 'max heartrate'],
                 fill_color='paleturquoise',
                 align='left'),
-    cells=dict(values=[lap_data.name, lap_data.moving_time, lap_data.total_elevation_gain, lap_data.average_speed, lap_data.average_heartrate, lap_data.max_heartrate],
+    cells=dict(values=[lap_data.name, lap_data.distance, lap_data.moving_time, lap_data.total_elevation_gain, lap_data.average_speed, lap_data.average_heartrate, lap_data.max_heartrate],
                fill_color='lavender',
                align='left'))
     ])
